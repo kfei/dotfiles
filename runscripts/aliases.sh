aa03f356ba26cce4d3618d678df02e0a9409e258
kfei <kfei@kfei.net>
2013-07-28 15:44:33 +0800

    Basic aliases for daily usage

---
+++ runscripts/aliases.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# some trivial aliases
+alias c='clear'
+alias ls='/bin/ls --color=auto'
+alias ll='/bin/ls -alh --color=auto'
+alias ..='cd ..'
+alias ....='cd ../..'
+alias ......='cd ../../../'
+alias mkdir='mkdir -pv'
+alias mount='mount | column -t'
+alias h='history'
+alias j='jobs -l'
+alias path='echo -e ${PATH//:/\\n}'
+alias ports='netstat -tulanp'
+
+# shortcut  for iptables and pass it via sudo#
+alias ipt='sudo /sbin/iptables'
+ 
+# display all rules #
+alias iptlist='sudo /sbin/iptables -L -n -v --line-numbers'
+alias iptlistin='sudo /sbin/iptables -L INPUT -n -v --line-numbers'
+alias iptlistout='sudo /sbin/iptables -L OUTPUT -n -v --line-numbers'
+alias iptlistfw='sudo /sbin/iptables -L FORWARD -n -v --line-numbers'
+alias firewall=iptlist
+
+# also pass it via sudo so whoever is admin can reload it without calling you #
+alias nginxreload='sudo /usr/local/nginx/sbin/nginx -s reload'
+alias nginxtest='sudo /usr/local/nginx/sbin/nginx -t'
+alias lightyload='sudo /etc/init.d/lighttpd reload'
+alias lightytest='sudo /usr/sbin/lighttpd -f /etc/lighttpd/lighttpd.conf -t'
+alias httpdreload='sudo /usr/sbin/apachectl -k graceful'
+alias httpdtest='sudo /usr/sbin/apachectl -t && /usr/sbin/apachectl -t -D DUMP_VHOSTS'
+
+# pass options to free #
+alias free='/usr/bin/free -m -l -t'
