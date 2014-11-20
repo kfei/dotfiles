36abedae8f09cd72022752e6f603f34d2fc39701
kfei <kfei@kfei.net>
2014-11-20 14:09:56 +0800

    Add an alias for lsblk
    Signed-off-by: kfei <kfei@kfei.net>

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -50,3 +50,4 @@ alias wwwhere='sudo python -m SimpleHTTPServer ' # e.g. wwwhere 8888
 alias vncsrv='x11vnc -nopw -display :0.0 -noremote -xinerama -clip xinerama0 -viewonly -forever -shared'
 alias googledns='sudo sh -c " echo '\''nameserver 8.8.8.8'\'' > /etc/resolv.conf "'
 alias html2rsync='rsync -avh --progress --exclude=".*" --include="*.html" --include="*/" --exclude="*"'
+alias lb='lsblk -o name,label,size,fstype,model'
